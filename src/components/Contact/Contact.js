952e84b6f75e8ac5c63c88fa2bb768ca05a64b03
1trackprojects1 <track.contact.email@gmail.com>
2021-04-25 18:55:22 +0530

    fixed padding issue and updated form for contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,15 +4,31 @@ import Header from "./Header/Header";
 const Contact = () => {
     return (
         <>
-            <div className="min-h-screen flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+            <div className="flex flex-col justify-center py-6 sm:px-6 lg:px-8">
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Contact Us</h2>
                 <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
                     <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
                         <form className="space-y-6" action="#" method="POST">
                             <div>
+                                <label htmlFor="fullName" className="block text-sm font-medium text-gray-700">
+                                    Full name:
+                                </label>
+                                <div className="mt-1">
+                                    <input
+                                        id="fullName"
+                                        name="fullName"
+                                        type="text"
+                                        autoComplete="name"
+                                        required
+                                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                    />
+                                </div>
+                            </div>
+
+                            <div>
                                 <label htmlFor="email" className="block text-sm font-medium text-gray-700">
                                     Email address
-                            </label>
+                                </label>
                                 <div className="mt-1">
                                     <input
                                         id="email"
@@ -26,38 +42,17 @@ const Contact = () => {
                             </div>
 
                             <div>
-                                <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                                    Password
-              </label>
+                                <label htmlFor="message" className="block text-sm font-medium text-gray-700">
+                                    Your message:
+                                </label>
                                 <div className="mt-1">
-                                    <input
-                                        id="password"
-                                        name="password"
-                                        type="password"
-                                        autoComplete="current-password"
+                                    <textarea
+                                        id="message"
+                                        rows="4"
+                                        cols="50"
                                         required
                                         className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                     />
-                                </div>
-                            </div>
-
-                            <div className="flex items-center justify-between">
-                                <div className="flex items-center">
-                                    <input
-                                        id="remember_me"
-                                        name="remember_me"
-                                        type="checkbox"
-                                        className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                                    />
-                                    <label htmlFor="remember_me" className="ml-2 block text-sm text-gray-900">
-                                        Remember me
-                </label>
-                                </div>
-
-                                <div className="text-sm">
-                                    <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                                        Forgot your password?
-                </a>
                                 </div>
                             </div>
 
@@ -67,7 +62,7 @@ const Contact = () => {
                                     className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                 >
                                     Contact Us
-              </button>
+                                </button>
                             </div>
                         </form>
                     </div>
