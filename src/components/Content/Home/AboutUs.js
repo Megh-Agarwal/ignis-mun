3dd8ea15b89726a004ed87576356442106e9e2e7
RC2408 <81439931+RC2408@users.noreply.github.com>
2021-04-28 20:18:32 +0530

    Bug fixes

--- src/components/Content/Home/AboutUs.js
+++ src/components/Content/Home/AboutUs.js
@@ -1,5 +1,3 @@
-import Logo from '../../../images/logo.jpeg';
-
 const AboutUs = () => {
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
