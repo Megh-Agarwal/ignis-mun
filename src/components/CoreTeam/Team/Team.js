187a65582d8f16405abb7c09f40a5d00f630e798
1trackprojects1 <track.contact.email@gmail.com>
2021-05-03 18:57:40 +0530

    added data fetch from api to core team and fixed minor errors (removed old Team.js)

--- src/components/CoreTeam/Team/Team.js
+++ src/components/CoreTeam/Team/Team.js
@@ -40,17 +40,6 @@ export const Team = () => {
                         setModalState(false);
                     }}>
                         <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-                            <Transition.Child
-                                as={Fragment}
-                                enter="ease-out duration-300"
-                                enterFrom="opacity-0"
-                                enterTo="opacity-100"
-                                leave="ease-in duration-200"
-                                leaveFrom="opacity-100"
-                                leaveTo="opacity-0"
-                            >
-                                <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-                            </Transition.Child>
 
                             {/* This element is to trick the browser into centering the modal contents. */}
                             <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
@@ -65,7 +54,7 @@ export const Team = () => {
                                 leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                             >
-                                <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
+                                <div className="inline-block align-bottom bg-gray-50 rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
                                     <div>
                                         <div className="mt-3 text-center sm:mt-5">
                                             <div className="mt-2">
